5c47b9e3894f2e3ff31d4eb39716f51cfc95db81
takrim1999 <takrimulalom@gmail.com>
2025-11-19 23:55:50 +0600

    learning about array type safety

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
-let age: number = 26
-console.log(age);
+let name = "Takrim";
+let age = 26;
+let anything;
+anything = 26;
+console.log(anything);
+anything = "Takrim";
+console.log(anything);
+let numbers: str[]|number[] = [1,2,'3'];
+console.log(numbers)
